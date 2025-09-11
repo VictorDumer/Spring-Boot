343ad6ccd75ab80f308f182267e3be8c07e68fec
VictorDumer <victorgabrieldumersouza@gmail.com>
2025-09-11 17:28:39 -0300

    w.i.p 1-working on

--- api-produtos/src/main/java/br/com/etecbentoquirino/api_produtos/ApiProdutosApplication.java
+++ api-produtos/src/main/java/br/com/etecbentoquirino/api_produtos/ApiProdutosApplication.java
@@ -2,7 +2,10 @@ package br.com.etecbentoquirino.api_produtos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.etecbentoquirino.api_produtos.model.Produto;
 
-    
+@SpringBootApplication
+public class ApiProdutosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiProdutosApplication.class, args);
+    }
+}
--- api-produtos/src/test/java/br/com/etecbentoquirino/api_produtos/ApiProdutosApplicationTests.java
+++ api-produtos/src/test/java/br/com/etecbentoquirino/api_produtos/ApiProdutosApplicationTests.java
@@ -3,11 +3,9 @@ package br.com.etecbentoquirino.api_produtos;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = br.com.etecbentoquirino.api_produtos.ApiProdutosApplication.class)
 class ApiProdutosApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
